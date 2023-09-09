3c0217b940feb4c62d2c1861009cb8da61f27a21
Shubhangi <shubhangisuman25@gmail.com>
2023-09-09 19:10:33 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(text,shift,direction):
+  alpha =''
+  for i in range(0,len(text)):
+    index = alphabet.index(text[i])
+    if(direction == "encode"):
+      shifted_index = index + shift
+      if(shifted_index > 25):
+        shifted_index = shifted_index-26
+      alpha += alphabet[shifted_index]
+      
+    elif (direction == "decode"):
+      deshift_index = index - shift
+      if(deshift_index < 0):
+        deshift_index= 26+deshift_index
+      alpha += alphabet[deshift_index]
+
+  print(f"The {direction}d text is: {alpha}")
+
+import art
+print(art.logo)
+      
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+caesar(text=text,shift=shift,direction=direction)
